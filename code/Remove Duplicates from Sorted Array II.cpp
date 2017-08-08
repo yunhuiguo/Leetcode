95894f519f581a7dbc9d67c8d439d3fd633a0fa6
yunhuiguo <gyhui@zju.edu.cn>
2017-08-08 14:04:29 +0800

    update

---
+++ code/Remove Duplicates from Sorted Array II.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        
+    }
+};
+
+
+int main() {
+    Solution s;
+    vector<int> nums1;
+
+    nums1.push_back(1);
+    nums1.push_back(3);
+    nums1.push_back(5);
+
+    
+
+    s.merge(nums1, 3, nums2, 3);
+    for(int i = 0; i < 6; i++)
+        cout << nums1[i] << endl;
+
+
+    return 0;
+}
